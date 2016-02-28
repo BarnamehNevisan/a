defb6b4f010c0c4c6bff529d4386dd04df7a27d3
es <barnameh.nevisan.2016@gmail.com>
2016-02-28 20:20:31 +0330

    Create spam..lua

---
+++ plugins/spam..lua
@@ -0,0 +1,18 @@
+do
+
+function run(msg, matches)
+  return [[
+spam.spam
+   ]]
+
+  end
+return {
+  description = "spam only", 
+  usage = "/start : return spams",
+  patterns = {
+    "^[/]start$",
+
+  },
+  run = run
+}
+end
